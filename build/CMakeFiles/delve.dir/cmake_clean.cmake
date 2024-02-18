9866c694bfc8e2f8babdf637f785cbc1c299f3d6
Alday <thomasconrod@gmail.com>
2024-02-18 23:34:26 +0000

    fixed CMakeLists.txt

---
+++ build/CMakeFiles/delve.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/delve.dir/src/main.cpp.o"
+  "CMakeFiles/delve.dir/src/main.cpp.o.d"
+  "delve"
+  "delve.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/delve.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
