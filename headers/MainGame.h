48952bd8403d23aedd1d2ce9ae12723328a56127
Alday <thomasconrod@gmail.com>
2024-03-12 23:49:19 +0000

    fps limiter/input manager

--- headers/MainGame.h
+++ headers/MainGame.h
@@ -2,7 +2,10 @@
 #include "../rogue_engine/headers/Camera2D.h"
 #include "../rogue_engine/headers/GLSLProgram.h"
 #include "../rogue_engine/headers/GLTexture.h"
+#include "../rogue_engine/headers/InputManager.h"
 #include "../rogue_engine/headers/Sprite.h"
+#include "../rogue_engine/headers/SpriteBatch.h"
+#include "../rogue_engine/headers/Timing.h"
 #include "../rogue_engine/headers/Window.h"
 #include "../rogue_engine/headers/rogue_engine.h"
 #include <GL/glew.h>
@@ -24,20 +27,21 @@ private:
   void gameLoop();
   void processInput();
   void drawGame();
-  void calculateFPS();
 
   rogue_engine::Window _window;
   int _screenWidth;
   int _screenHeight;
   GameState _gameState;
 
-  std::vector<rogue_engine::Sprite *> _sprites;
   rogue_engine::GLSLProgram _colorProgram;
   rogue_engine::Camera2D _camera;
 
+  rogue_engine::SpriteBatch _spriteBatch;
+
+  rogue_engine::InputManager _inputManager;
+  rogue_engine::FpsLimiter _fpsLimiter;
+
   float _fps;
   float _maxFPS;
-  float _frameTime;
-
   float _time;
 };
--- src/MainGame.cpp
+++ src/MainGame.cpp
@@ -1,6 +1,8 @@
 #include "../headers/MainGame.h"
+#include "../rogue_engine/headers/ResourceManager.h"
 #include "../rogue_engine/headers/rogue_engine.h"
 
+#include <SDL2/SDL_events.h>
 #include <SDL2/SDL_timer.h>
 #include <SDL2/SDL_video.h>
 #include <iostream>
@@ -8,7 +10,7 @@
 MainGame::MainGame()
     // init list
     : _screenWidth(1024), _screenHeight(768), _time(0),
-      _gameState(GameState::PLAY), _maxFPS(60.0f) {
+      _gameState(GameState::PLAY), _maxFPS(60.0) {
   _camera.init(_screenWidth, _screenHeight);
 }
 
@@ -16,17 +18,6 @@ MainGame::~MainGame() {}
 
 void MainGame::run() {
   initSystems();
-  // init sprites
-  _sprites.push_back(new rogue_engine::Sprite());
-  _sprites.back()->init(
-      0.0f, 0.0f, _screenWidth / 2.0, _screenHeight / 2.0,
-      "../assets/0x72_DungeonTilesetII_v1.6/frames/wizzard_m_idle_anim_f0.png");
-
-  _sprites.push_back(new rogue_engine::Sprite());
-  _sprites.back()->init(
-      _screenWidth / 2.0, 0.0f, _screenWidth / 2.0, _screenHeight / 2.0,
-      "../assets/0x72_DungeonTilesetII_v1.6/frames/wizzard_m_idle_anim_f0.png");
-
   gameLoop();
 }
 
@@ -34,9 +25,13 @@ void MainGame::run() {
 void MainGame::initSystems() {
 
   rogue_engine::init();
+
   _window.create("Game Engine", _screenWidth, _screenHeight, 0);
 
   initShaders();
+
+  _spriteBatch.init();
+  _fpsLimiter.init(_maxFPS);
 }
 
 void MainGame::initShaders() {
@@ -52,8 +47,7 @@ void MainGame::initShaders() {
 void MainGame::gameLoop() {
   while (_gameState != GameState::EXIT) {
 
-    // used for frame time measuring
-    float startTicks = SDL_GetTicks();
+    _fpsLimiter.begin();
 
     processInput();
     _time += 0.01;
@@ -61,8 +55,7 @@ void MainGame::gameLoop() {
     _camera.update();
 
     drawGame();
-
-    calculateFPS();
+    _fps = _fpsLimiter.end();
 
     // print once every ten frames
     static int frameCounter = 0;
@@ -71,12 +64,6 @@ void MainGame::gameLoop() {
       std::cout << _fps << std::endl;
       frameCounter = 0;
     }
-
-    // limit the FPS to the max fps
-    float frameTicks = SDL_GetTicks() - startTicks;
-    if (1000.0f / _maxFPS > frameTicks) {
-      SDL_Delay((1000.0f / _maxFPS) - frameTicks);
-    }
   }
 };
 
@@ -84,7 +71,7 @@ void MainGame::gameLoop() {
 void MainGame::processInput() {
   SDL_Event evnt;
 
-  const float CAMERA_SPEED = 20.0f;
+  const float CAMERA_SPEED = 2.0f;
   const float SCALE_SPEED = 0.1f;
 
   // while the user is making inputs:
@@ -95,40 +82,36 @@ void MainGame::processInput() {
     // return 0 from GameState
     case SDL_QUIT:
       _gameState = GameState::EXIT;
-
     // track the position of the mouse in SDL.
     case SDL_MOUSEMOTION:
       // std::cout << evnt.motion.x << " " << evnt.motion.y << std::endl;
       break;
     case SDL_KEYDOWN:
-      switch (evnt.key.keysym.sym) {
-      case SDLK_w:
-        _camera.setPosition(_camera.getPosition() +
-                            glm::vec2(0.0, CAMERA_SPEED));
-        break;
-      case SDLK_s:
-        _camera.setPosition(_camera.getPosition() +
-                            glm::vec2(0.0, -CAMERA_SPEED));
-        break;
-      case SDLK_a:
-        _camera.setPosition(_camera.getPosition() +
-                            glm::vec2(-CAMERA_SPEED, 0.0));
-        break;
-      case SDLK_d:
-        _camera.setPosition(_camera.getPosition() +
-                            glm::vec2(CAMERA_SPEED, 0.0));
-        break;
-      case SDLK_q:
-        _camera.setScale(_camera.getScale() + SCALE_SPEED);
-        break;
-      case SDLK_e:
-        _camera.setScale(_camera.getScale() - SCALE_SPEED);
-        break;
-      }
+      _inputManager.pressKey(evnt.key.keysym.sym);
       break;
+    case SDL_KEYUP:
+      _inputManager.releaseKey(evnt.key.keysym.sym);
     }
   };
-};
+  if (_inputManager.isKeyPressed(SDLK_w)) {
+    _camera.setPosition(_camera.getPosition() + glm::vec2(0.0f, CAMERA_SPEED));
+  }
+  if (_inputManager.isKeyPressed(SDLK_s)) {
+    _camera.setPosition(_camera.getPosition() + glm::vec2(0.0f, -CAMERA_SPEED));
+  }
+  if (_inputManager.isKeyPressed(SDLK_a)) {
+    _camera.setPosition(_camera.getPosition() + glm::vec2(-CAMERA_SPEED, 0.0f));
+  }
+  if (_inputManager.isKeyPressed(SDLK_d)) {
+    _camera.setPosition(_camera.getPosition() + glm::vec2(CAMERA_SPEED, 0.0f));
+  }
+  if (_inputManager.isKeyPressed(SDLK_q)) {
+    _camera.setScale(_camera.getScale() + SCALE_SPEED);
+  }
+  if (_inputManager.isKeyPressed(SDLK_e)) {
+    _camera.setScale(_camera.getScale() - SCALE_SPEED);
+  }
+}
 
 void MainGame::drawGame() {
   // make sure the screen is clear before drawing
@@ -156,10 +139,29 @@ void MainGame::drawGame() {
 
   glUniformMatrix4fv(pLocation, 1, GL_FALSE, &(cameraMatrix[0][0]));
 
-  // draw sprites
-  for (int i = 0; i < _sprites.size(); i++) {
-    _sprites[i]->draw();
+  _spriteBatch.begin();
+
+  glm::vec4 pos(0.0f, 0.0f, 50.0f, 50.0f);
+  glm::vec4 uv(0.0f, 0.0f, 1.0f, 1.0f);
+
+  static rogue_engine::GLTexture texture =
+      rogue_engine::ResourceManager::getTexture(
+          "/home/matosade/Projects/Game_Dev/delve/assets/"
+          "0x72_DungeonTilesetII_v1.6/frames/wizzard_m_idle_anim_f0.png");
+  rogue_engine::Color color;
+  color.r = 255;
+  color.g = 255;
+  color.b = 255;
+  color.a = 255;
+
+  for (int i = 0; i < 2; i++) {
+    _spriteBatch.draw(pos, uv, texture.id, 0.0f, color);
+    _spriteBatch.draw(pos + glm::vec4(50, 0, 0, 0), uv, texture.id, 0.0f,
+                      color);
   }
+  _spriteBatch.end();
+
+  _spriteBatch.renderBatch();
 
   // unbind
   glBindTexture(GL_TEXTURE_2D, 0);
@@ -168,40 +170,3 @@ void MainGame::drawGame() {
   // swap our buffer before drawing
   _window.swapBuffer();
 };
-
-// fps counter
-void MainGame::calculateFPS() {
-  static const int NUM_SAMPLES = 10;
-  static float frameTimes[NUM_SAMPLES];
-  static int currentFrame = 0;
-
-  static float prevTicks = SDL_GetTicks();
-  float currentTicks;
-  currentTicks = SDL_GetTicks();
-
-  _frameTime = currentTicks - prevTicks;
-  frameTimes[currentFrame % NUM_SAMPLES] = _frameTime;
-
-  prevTicks = currentTicks;
-
-  int count;
-
-  currentFrame++;
-  if (currentFrame < NUM_SAMPLES) {
-    count = currentFrame;
-  } else {
-    count = NUM_SAMPLES;
-  }
-
-  float frameTimeAverage = 0;
-  for (int i = 0; i < count; i++) {
-    frameTimeAverage += frameTimes[i];
-  }
-  frameTimeAverage /= count;
-
-  if (frameTimeAverage > 0) {
-    _fps = 1000.0f / frameTimeAverage;
-  } else {
-    _fps = 60.0f;
-  }
-}
